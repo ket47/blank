dd8d9c77641c1ee0eeba6302184213d54d243ca9
Bayci <baycikplus@gmail.com>
2022-09-02 14:21:41 +0300

    google index

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -57,11 +57,11 @@ ion-icon{
               <ion-icon :icon="cartOutline" slot="start" color="primary"></ion-icon>
               <ion-label>Мои заказы</ion-label>
           </ion-item>
-          <ion-item lines="full" button detail @click="$router.push('user-promo')">
+          <ion-item lines="full" button detail @click="$router.push('/user/user-promo')">
               <ion-icon :icon="giftOutline" slot="start" color="primary"></ion-icon>
               <ion-label>Мои скидки</ion-label>
           </ion-item>
-          <ion-item lines="full" button detail @click="$router.push('user-invoice')">
+          <ion-item lines="full" button detail @click="$router.push('/user/user-invoice')">
               <ion-icon :icon="receiptOutline" slot="start" color="primary"></ion-icon>
               <ion-label>Мои чеки</ion-label>
           </ion-item>
@@ -215,19 +215,19 @@ ion-icon{
             <ion-icon :icon="informationCircleOutline" slot="start" color="primary"></ion-icon>
             <ion-text>О нас</ion-text>
         </ion-item>
-        <ion-item lines="full" button detail @click="$router.push('page-rules-customer')">
+        <ion-item lines="full" button detail @click="$router.push('/page-rules-customer')">
             <ion-icon :icon="informationCircleOutline" slot="start" color="primary"></ion-icon>
             <ion-text>Правила пользования</ion-text>
         </ion-item>
-        <ion-item lines="full" button detail @click="$router.push('page-rules-supplier')">
+        <ion-item lines="full" button detail @click="$router.push('/page-rules-supplier')">
             <ion-icon :icon="informationCircleOutline" slot="start" color="primary"></ion-icon>
             <ion-text>Правила пользования для продавца</ion-text>
         </ion-item>
-        <ion-item lines="full" button detail @click="$router.push('page-rules-courier')">
+        <ion-item lines="full" button detail @click="$router.push('/page-rules-courier')">
             <ion-icon :icon="informationCircleOutline" slot="start" color="primary"></ion-icon>
             <ion-text>Правила пользования для курьера</ion-text>
         </ion-item>
-        <ion-item lines="full" button detail @click="$router.push('page-privacy_policy')">
+        <ion-item lines="full" button detail @click="$router.push('/page-privacy_policy')">
             <ion-icon :icon="informationCircleOutline" slot="start" color="primary"></ion-icon>
             <ion-text>Политика конфиденциальности</ion-text>
         </ion-item>
