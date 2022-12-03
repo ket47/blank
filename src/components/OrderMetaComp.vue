230f30f347478d6dcd88365a85e39f478d1a10bb
Bayci <baycikplus@gmail.com>
2022-12-03 16:45:13 +0300

    bugfixes

--- src/components/OrderMetaComp.vue
+++ src/components/OrderMetaComp.vue
@@ -5,19 +5,19 @@
                 <ion-label>Дополнительно</ion-label>
             </ion-item>
             <div slot="content" class="ion-padding">
-                <ion-chip v-if="meta.payment_card_fixate_sum>0" color="primary">
+                <ion-chip v-if="meta.payment_card_fixate_sum>0">
                     <ion-icon :src="cardOutline"/>
                     <ion-text>Блок {{meta.payment_card_fixate_sum}}{{$heap.state.currencySign}}</ion-text>
                 </ion-chip>
-                <ion-chip v-if="meta.payment_card_refund_sum>0" color="primary">
+                <ion-chip v-if="meta.payment_card_refund_sum>0">
                     <ion-icon :src="cardOutline"/> 
-                    <ion-text>Разблок. {{meta.payment_card_refund_sum}}{{$heap.state.currencySign}}</ion-text>
+                    <ion-text>Разблокировано {{meta.payment_card_refund_sum}}{{$heap.state.currencySign}}</ion-text>
                 </ion-chip>
-                <ion-chip v-if="meta.payment_card_confirm_sum>0" color="primary">
+                <ion-chip v-if="meta.payment_card_confirm_sum>0">
                     <ion-icon :src="cardOutline"/>
-                    <ion-text>Опл. {{meta.payment_card_confirm_sum}}{{$heap.state.currencySign}}</ion-text>
+                    <ion-text>Оплачено {{meta.payment_card_confirm_sum}}{{$heap.state.currencySign}}</ion-text>
                 </ion-chip>
-                <ion-chip v-if="meta.delivery_by_courier||meta.delivery_by_store||meta.pickup_by_customer " color="primary">
+                <ion-chip v-if="meta.delivery_by_courier||meta.delivery_by_store||meta.pickup_by_customer ">
                     <ion-icon :src="rocketOutline"/>
                     <ion-text>
                     <span v-if="meta.delivery_by_courier">Курьер</span>
@@ -25,9 +25,9 @@
                     <span v-if="meta.pickup_by_customer">Самовывоз</span>
                     </ion-text>
                 </ion-chip>
-                <ion-button v-if="meta.invoice_link" color="light" @click="billOpen(meta.invoice_link)">
+                <ion-button v-if="meta.invoice_link" color="light" @click="billOpen(meta.invoice_link)" expand="block">
                     <ion-icon :src="receiptOutline" slot="start"/>
-                    <ion-text>Открыть чек {{meta.invoice_sum}}{{$heap.state.currencySign}}</ion-text>
+                    <ion-text>Открыть чек на {{meta.invoice_sum}}{{$heap.state.currencySign}}</ion-text>
                 </ion-button>
                 <ion-list v-if="ledgerCalc.length>0">
                     <ion-item v-for="trans in ledgerCalc" :key="trans.trans_id">
