2775cab11f72f3e3b02df865836a4d52d090f301
Bayci <baycikplus@gmail.com>
2023-02-06 16:12:17 +0300

    bugfixes

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,5 +1,5 @@
 <template>
-    <base-layout :pageTitle="`Заказ #${order_id}`" pageDefaultBackLink="/order/order-list">
+    <base-layout :pageTitle="`Заказ #${order_id} ${order?.deleted_at?'(Удален)':''}` " pageDefaultBackLink="/order/order-list">
 
             <div v-if="order=='notfound'" style="display:flex;align-items:center;justify-content:center;height:100%">
                 <div style="width:max-content;text-align:center">
