1ae5c68dc562cd36f6ed67acf62982e3ad0542ab
AdamHider <ajder.adjivapov@gmail.com>
2024-05-25 18:18:25 +0300

    delivery variant height fix

---
+++ src/views/admin/AnalyticsView.vue
@@ -0,0 +1,33 @@
+<template>
+    <base-layout pageTitle="Статистика продавца" pageDefaultBackLink="/user">
+        
+    </base-layout>
+</template>
+
+<script>
+import StatisticsCard          from "@/components/StatisticsCard.vue";
+
+import jQuery       from "jquery";
+
+export default {
+    components:{
+        StatisticsCard
+    },
+    setup(){
+        return {}
+    },
+    data(){
+        return {
+            analysis:null,
+        }
+    },
+    mounted(){
+        this.analysisGet()
+    },
+    methods:{
+        async analysisGet(){
+            this.analysis = await jQuery.post(`${this.$heap.state.hostname}Statistics/totalOrderAnalysisGet`, {point_span: 7, point_num: 10})
+        },
+    },
+}
+</script>
